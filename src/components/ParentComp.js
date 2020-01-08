1524bb0636fb5e1b9c30517a7d7cd00a7d2d169c
Chandana priya Kokkarakonda <chandanapriyak@Virtusa.com>
2020-01-08 19:03:58 +0530

    new1 commit

--- src/components/ParentComp.js
+++ src/components/ParentComp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import RegComp from './RegComp'
 import PureComp from './PureComp'
+import MemoComp from './MemoComp'
 
 class ParentComp extends Component {
     constructor(props) {
@@ -27,8 +28,9 @@ class ParentComp extends Component {
         return (
             <div>
                 Parent Component
-                <RegComp name={this.state.name}></RegComp>
-                <PureComp name={this.state.name}></PureComp>
+                <MemoComp name={this.state.name}></MemoComp>
+                {/* <RegComp name={this.state.name}></RegComp>
+                <PureComp name={this.state.name}></PureComp> */}
             </div>
         )
     }
